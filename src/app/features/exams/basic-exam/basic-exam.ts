8e38eaa5079c0a7743795bcc8532e2d06f3e662a
David <dwoof42@gmail.com>
2026-04-10 12:09:18 -0700

    add first page

---
+++ src/app/features/exams/basic-exam/basic-exam.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-exam',
+  imports: [],
+  templateUrl: './basic-exam.html',
+  styleUrl: './basic-exam.css',
+})
+export class BasicExam {}
